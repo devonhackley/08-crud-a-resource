4821634bb8c9efd4f359c18d3e391d3262d5fc8e
devonhackley <devonhackley@gmail.com>
2016-12-07 16:14:13 -0800

    first couple of todos done

--- starter-code/scripts/models/article.js
+++ starter-code/scripts/models/article.js
@@ -20,7 +20,7 @@
   Article.createTable = function() {
     // webDb helps us query our data
     webDB.execute(
-      '', // TODO: What SQL command do we run here inside these quotes?
+      'CREATE TABLE IF NOT EXIST books (id INTEGER PRIMARY KEY, title VARCHAR, catagory VARCHAR, author VARCHAR, authorURL VARCHA, published-on VARCHAR, body VARCHAR )', // TODONE: What SQL command do we run here inside these quotes?
       function() {
         console.log('Successfully set up the articles table.');
       }
@@ -38,7 +38,7 @@
     webDB.execute(
       [{
         // NOTE: this method will be called elsewhere after we retrieve our JSON
-        'sql': '', // <----- TODO: complete our SQL query here, inside the quotes.
+        'sql': 'INSERT INTO books (title,category,author,authorURL,publishedOn,body) VALUES (?,?,?,?,?,?)', // <----- TODONE: complete our SQL query here, inside the quotes.
         'data': [this.title, this.category, this.author, this.authorUrl, this.publishedOn, this.body]
       }]
     );
@@ -46,13 +46,16 @@
 
   Article.fetchAll = function(nextFunction) {
     webDB.execute(
-      '', // <-----TODO: fill these quotes to query our table.
+      'SELECT * FROM books', // <-----TODONE: fill these quotes to query our table.
       function(rows) {
         // if we have data in the table
         if (rows.length) {
-        /* TODO:
+        /* TODONE:
            1 - Use Article.loadAll to instanitate these rows,
+
            2 - invoke the function that was passed in to fectchAll */
+          Article.loadAll(rows);
+          rows.nextFunction();
         } else {
           $.getJSON('/data/hackerIpsum.json', function(responseData) {
             responseData.forEach(function(obj) {
